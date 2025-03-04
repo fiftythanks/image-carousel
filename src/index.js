b3eacef7ea26c1a54d56c0b3a78b252ba3cc57b8
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-03-04 12:46:51 +0700

    Finish project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import './style.css';
 const slides = document.querySelector('.slides');
 const dots = Array.from(document.querySelector('.dots').children);
 
-let timeout;
-let interval;
-let isLast;
-
 function slideLeft() {
   const offset = slides.style.left;
   switch (offset) {
@@ -14,25 +10,21 @@ function slideLeft() {
       slides.style.left = '-2400px';
       dots[3].classList.toggle('current-slide');
       dots[2].classList.toggle('current-slide');
-      isLast = false;
       break;
     case '-2400px':
       slides.style.left = '-1200px';
       dots[2].classList.toggle('current-slide');
       dots[1].classList.toggle('current-slide');
-      isLast = false;
       break;
     case '-1200px':
       slides.style.left = '0px';
       dots[1].classList.toggle('current-slide');
       dots[0].classList.toggle('current-slide');
-      isLast = false;
       break;
     case '0px':
       slides.style.left = '-3600px';
       dots[0].classList.toggle('current-slide');
       dots[3].classList.toggle('current-slide');
-      isLast = true;
       break;
     default:
     // do nothing
@@ -46,36 +38,31 @@ function slideRight() {
       slides.style.left = '-1200px';
       dots[0].classList.toggle('current-slide');
       dots[1].classList.toggle('current-slide');
-      isLast = false;
-      interval = setInterval(slideRight, 5000);
       break;
     case '-1200px':
       slides.style.left = '-2400px';
       dots[1].classList.toggle('current-slide');
       dots[2].classList.toggle('current-slide');
-      isLast = false;
       break;
     case '-2400px':
       slides.style.left = '-3600px';
       dots[2].classList.toggle('current-slide');
       dots[3].classList.toggle('current-slide');
-      isLast = true;
-      clearInterval(interval);
       break;
     case '-3600px':
       slides.style.left = '0px';
       dots[3].classList.toggle('current-slide');
       dots[0].classList.toggle('current-slide');
-      isLast = false;
       break;
     default:
     // do nothing
   }
 }
 
-timeout = setTimeout(slideRight, 5000);
-
-const isStopped = false; // for dots (when you click on one of them, the autosliding will stop)
+let interval;
+const timeout = setTimeout(() => {
+  interval = setInterval(slideRight, 5000);
+}, 5000);
 
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
@@ -84,11 +71,43 @@ left.addEventListener('click', () => {
   slideLeft();
   clearTimeout(timeout);
   clearInterval(interval);
-  if (!isLast && !isStopped) timeout = setTimeout(slideRight(true), 5000);
 });
 right.addEventListener('click', () => {
   slideRight();
   clearTimeout(timeout);
   clearInterval(interval);
-  if (!isLast && !isStopped) timeout = setTimeout(slideRight(true), 5000);
 });
+
+for (let i = 0; i < dots.length; i += 1) {
+  // eslint-disable-next-line no-loop-func
+  dots[i].addEventListener('click', () => {
+    clearTimeout(timeout);
+    clearInterval(interval);
+    const dot = dots[i];
+    const selected = document.querySelector('.current-slide');
+    switch (i) {
+      case 0:
+        slides.style.left = '0px';
+        selected.classList.toggle('current-slide');
+        dot.classList.toggle('current-slide');
+        break;
+      case 1:
+        slides.style.left = '-1200px';
+        selected.classList.toggle('current-slide');
+        dot.classList.toggle('current-slide');
+        break;
+      case 2:
+        slides.style.left = '-2400px';
+        selected.classList.toggle('current-slide');
+        dot.classList.toggle('current-slide');
+        break;
+      case 3:
+        slides.style.left = '-3600px';
+        selected.classList.toggle('current-slide');
+        dot.classList.toggle('current-slide');
+        break;
+      default:
+      // do nothing
+    }
+  });
+}
